3899057349b01236c215e6e8d27d5366b674d243
Vil_Llar <35060606+dark-dekar@users.noreply.github.com>
2020-12-28 00:54:10 +0100

    feat(#4): add logic for show or hide heart icon

--- src/modules/ContentArea/components/VoiceItem.vue
+++ src/modules/ContentArea/components/VoiceItem.vue
@@ -1,5 +1,9 @@
 <template>
   <div :class="['voice-item', {'selected': isSelected}]" @click="toggleSelection()">
+      <div class="fav-img">
+        <img v-if="isFavorite" src="@/assets/voice-favorite.svg" alt="Favorite">
+        <img v-else src="@/assets/voice-favorite-off.svg" alt="Favorite">
+      </div>
       <div class="voice-icon">
           <img :src="getIcon(voice.icon)" :alt="voice.name">
       </div>
@@ -12,6 +16,7 @@ export default {
     name: 'VoiceItem',
     props: {
         voice: Object,
+        isFavorite: Boolean,
     },
     data: function () {
         return {
@@ -33,11 +38,24 @@ export default {
 @import '@/resources/styles/colors.scss';
 
 .voice-item {
+    position: relative;
     height: fit-content;
     text-align: center;
     margin: 0 20px 24px;
     cursor: pointer;
 
+    .fav-img {
+        display: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        background-color: $hint-color;
+        border-radius: 100%;
+        width: 26px;
+        height: 26px;
+        justify-content: center;
+        align-items: center;
+    }
     .voice-icon {
         background-color: $main-color;
         border-radius: 100%;
@@ -51,6 +69,9 @@ export default {
     }
 
     &:hover {
+        .fav-img {
+            display: flex;
+        }
         .voice-icon {
             background-color: $hint-color;
         }
