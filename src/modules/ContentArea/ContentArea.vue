4d5e63b913632b9fe542ab36627b19f1cc5ad886
Vil_Llar <35060606+dark-dekar@users.noreply.github.com>
2020-12-28 17:57:35 +0100

    feat(#4): change logic to ternary operator

--- src/modules/ContentArea/ContentArea.vue
+++ src/modules/ContentArea/ContentArea.vue
@@ -23,11 +23,7 @@ export default {
     favoriteVoices: function (favorites){
       const ids = favorites.map(fav => fav.id);
       this.voices.forEach(fav => {
-        if(ids.includes(fav.id)) {
-          fav.isFavorite = true;
-        } else {
-          fav.isFavorite = false;
-        }
+        fav.isFavorite = ids.includes(fav.id)? true : false;
       });
     }
   },
