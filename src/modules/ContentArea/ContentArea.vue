10beaa24cc00c73efce979926a941da2abd9ad0e
Vil_Llar <35060606+dark-dekar@users.noreply.github.com>
2020-12-28 23:15:59 +0100

    feat(#2): hide favorites when lsit is filtered

--- src/modules/ContentArea/ContentArea.vue
+++ src/modules/ContentArea/ContentArea.vue
@@ -1,43 +1,62 @@
 <template>
   <div class="content-area custom-scrollbar">
-      <VoiceCategory v-if="favoriteVoices && favoriteVoices.length > 0" :title="$t('categories.favorite')" :voices="favoriteVoices"/>
-      <VoiceCategory :title="$t('categories.pro')" :voices="voices"/>
+    <VoiceCategory
+      v-if="showFavorites && favoriteVoices && favoriteVoices.length > 0"
+      :title="$t('categories.favorite')"
+      :voices="favoriteVoices"
+    />
+    <VoiceCategory :title="$t('categories.pro')" :voices="voices" />
   </div>
 </template>
 
 <script>
-import VoiceCategory from './components/VoiceCategory';
-export default {
-  name: 'ContentArea',
-  data: function () {
-    return {
-      favoriteVoices:[],
-      voices: [],
-    }
-  },
-  created() {
-    this.favoriteVoices = this.$store.state.favorites;
-    this.voices = this.$voicesService.getAllVoices();
-  },
-  watch: {
-    favoriteVoices: function (favorites){
-      const ids = favorites.map(fav => fav.id);
-      this.voices.forEach(fav => {
-        fav.isFavorite = ids.includes(fav.id);
-      });
-    }
-  },
-  components: {
-    VoiceCategory
-  }
-}
+  import VoiceCategory from "./components/VoiceCategory";
+  export default {
+    name: "ContentArea",
+    data: function() {
+      return {
+        showFavorites: true,
+        favoriteVoices: [],
+        voices: [],
+      };
+    },
+    created() {
+      this.favoriteVoices = this.isFiltered(this.$route.query)
+        ? []
+        : this.$store.state.favorites;
+      this.voices = this.$voicesService.getAllVoices();
+    },
+    methods: {
+      isFiltered(queryParams) {
+        return !!queryParams.searchTerm;
+      },
+    },
+    watch: {
+      favoriteVoices: function(favorites) {
+        const ids = favorites.map((fav) => fav.id);
+        this.voices.forEach((fav) => {
+          fav.isFavorite = ids.includes(fav.id);
+        });
+      },
+      $route(to) {
+        if (this.isFiltered(to.query)) {
+          this.showFavorites = false;
+        } else {
+          this.showFavorites = true;
+        }
+      },
+    },
+    components: {
+      VoiceCategory,
+    },
+  };
 </script>
 
 <style lang="scss">
-.content-area {
-  overflow: auto;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
+  .content-area {
+    overflow: auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 </style>
