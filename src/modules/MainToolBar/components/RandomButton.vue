582c914a51dcf6bbf6885aeb8ce8c6336c1eff95
Vil_Llar <35060606+dark-dekar@users.noreply.github.com>
2020-12-30 15:24:40 +0100

    feat(#23): add random buton component

---
+++ src/modules/MainToolBar/components/RandomButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="random-button">
+    <img src="@/assets/button-random.svg" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RandomButton',
+    methods: {},
+  };
+</script>
+
+<style lang="scss">
+  @import '@/resources/styles/breakpoints.scss';
+
+  .random-button {
+    padding-left: 12px;
+  }
+
+  @media (max-width: $mobile-device) {
+    .random-button {
+      padding-left: 0;
+    }
+  }
+</style>
